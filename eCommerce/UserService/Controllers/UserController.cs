8e05487e22a10bab6def1eef5704933c73837976
Simon Alphonse <IAmSimon@outlook.in>
2023-03-02 11:41:13 +0530

    Added User Service

---
+++ eCommerce/UserService/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+using UserService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.UserService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager _userManager;
+
+        public UserController(UserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok(await _userManager.GetUsers());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            return Ok(await _userManager.GetUser(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(User user)
+        {
+            var addedUser = await _userManager.AddUser(user);
+            return CreatedAtAction("GetUser", new { id = addedUser.Id }, addedUser);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, User user)
+        {
+            if (id != user.Id) return BadRequest();
+            return Ok(await _userManager.UpdateUser(user));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            return Ok(await _userManager.DeleteUser(id));
+        }
+    }
+}
